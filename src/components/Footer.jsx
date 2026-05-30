de5ae0ad03e11fe93a5d8fb74391f7fffc447a7a
AadhyaCaterers <aadhyacaterers01@gmail.com>
2026-05-30 17:42:46 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -426,3 +426,14 @@ export default function Footer() {
     </footer>
   );
 }
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10797028884">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-10797028884');
+</script>
